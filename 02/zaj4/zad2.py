ed6a1a6bc4d7448b63bdbd603ac878888c5df054
krymsza <36919707+krymsza@users.noreply.github.com>
2018-03-21 12:57:03 +0100

    Create zad2.py

---
+++ 02/zaj4/zad2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+def podz(nazwa):
+	f = open(nazwa)
+	napis = f.read()
+	f.close()
+	napis = napis.split("\n")
+	slownik={}
+	for i in napis:
+		if i != "\n" and i != "":
+			a,b=i.split(":")
+			slownik[a]=b
+	return slownik
+
+print podz("zad2_test.txt")
